c12ca69c5f870bcb382a3e143b84256fea220626
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-09-25 04:56:49 +0300

    Fix fetching

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 function App() {
   return (
     <>
+      <h1>app</h1>
       <Home></Home>
     </>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,24 @@ import { useEffect, useState } from "react";
 const getPosts = async () => {
   const response = await fetch("http://localhost:8000/api/posts");
   const posts = await response.json();
+  // console.log("new", posts, posts.posts);
   return posts;
 };
 export default function Home() {
-  const [posts, setPosts] = useState();
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
-    setPosts(getPosts());
+    getPosts().then((data) => setPosts(data.posts));
   }, []);
-  return <h2>Home</h2>;
+  console.log("render", posts);
+  const postsDisplay = posts.map((post) => (
+    <div key={post.id}>
+      <h2>{post.title}</h2>
+    </div>
+  ));
+  return (
+    <>
+      <h1>Home</h1>
+      {postsDisplay}
+    </>
+  );
 }
